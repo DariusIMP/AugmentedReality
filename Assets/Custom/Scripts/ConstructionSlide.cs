767e6cd1a9b27a011dd4879629338395fea9f053
Juan Manuel Lambre <juanmlambre@gmail.com>
2019-06-25 15:35:04 -0300

    Created slides structure

---
+++ Assets/Custom/Scripts/ConstructionSlide.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructionSlide 
+{
+
+    private readonly List<GameObject> elements;
+    public string Name;
+    
+
+    public ConstructionSlide(string name, params GameObject[] elements)
+    {
+        this.elements = new List<GameObject>(collection: elements);
+        this.Name = name;
+    }
+
+    public void Hide()
+    {
+        foreach (GameObject element in elements)
+        {
+            if (elements != null)
+                element.SetActive(false);
+        }
+    }
+
+    public void Show()
+    {
+        foreach (GameObject element in elements)
+        {
+            if (element != null)
+                element.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Custom/Scripts/PlaneTrackableEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneTrackableEventHandler : DefaultTrackableEventHandler
+{
+
+    public GameObject slideButtons;
+
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        slideButtons.SetActive(true);
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        slideButtons.SetActive(false);
+    }
+}
