a93621b9100061589818fc8bd0c997f33ad50748
Darius <dariusmaitia@gmail.com>
2019-08-07 04:21:07 -0300

    Raycasting sobre las perillas y despliegue de informacion al posicionarse la orientación de la cámara sobre las perillas (proof of concept)

---
+++ Assets/Custom/Scripts/SelectionManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Custom.Scripts
+{
+    public class SelectionManager : MonoBehaviour
+    {
+        [SerializeField] private string selectableTag = "Selectable";
+        [SerializeField] private Material highlightMaterial;
+        [SerializeField] private Material defaultMaterial;
+        private GameObject selectedObject;
+        private Transform _selection;
+    
+        private void Update()
+        {
+            if (_selection != null)
+            {
+                var selectionRenderer = _selection.GetComponent<Renderer>();
+                selectionRenderer.material = defaultMaterial;
+                _selection = null;
+                selectedObject.GetComponent<DisplayMenu>().HideMenu();
+            }
+        
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform;
+                if (selection.CompareTag(selectableTag))
+                {
+                    var selectionRenderer = selection.GetComponent<Renderer>();
+                    if (selectionRenderer != null)
+                    {
+                        selectionRenderer.material = highlightMaterial;
+                        selectedObject = hit.collider.gameObject;
+                        selectedObject.GetComponent<DisplayMenu>().ShowMenu();
+                    }
+
+                    _selection = selection;
+                }
+            }
+        }
+    }
+}
