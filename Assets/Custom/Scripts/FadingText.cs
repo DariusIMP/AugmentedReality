0ff086edffafacd8260de8fa0f59b4147a2b8dd7
Darius <dariusmaitia@gmail.com>
2019-08-13 18:32:41 -0300

    Fading Effect para mostrar instrucciones al usuario

---
+++ Assets/Custom/Scripts/FadingText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Custom.Scripts
+{
+    public class FadingText : MonoBehaviour
+    {
+        private FadingEffects _fadingEffects;
+        
+        public String textToShow;
+        
+        public float holdingTime, fadingTime;
+
+        public void Start()
+        {
+            _fadingEffects = gameObject.AddComponent<FadingEffects>();
+        }
+
+        public void ShowFadingText()
+        {
+            Text titulo = gameObject.GetComponent<Text>();
+            titulo.text = textToShow;
+            StartCoroutine(_fadingEffects.ShowAndHideTextFading(fadingTime, holdingTime, titulo));
+        }
+    }
+}
