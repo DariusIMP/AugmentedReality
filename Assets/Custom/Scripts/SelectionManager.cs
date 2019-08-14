788f1dd94e16cb59dd8be20978df1c483d4fbf46
Darius <dariusmaitia@gmail.com>
2019-08-13 21:08:21 -0300

    Funcionalidad de lock de la información

--- Assets/Custom/Scripts/SelectionManager.cs
+++ Assets/Custom/Scripts/SelectionManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Custom.Scripts
 {
@@ -9,35 +10,60 @@ namespace Custom.Scripts
         [SerializeField] private Material defaultMaterial;
         private GameObject selectedObject;
         private Transform _selection;
+        private bool selectionLocked;
+        
         public Camera camera;
+        public GameObject aimingDot;
     
         private void Update()
         {
-            if (_selection != null)
+            if (!selectionLocked)
             {
-                var selectionRenderer = _selection.GetComponent<Renderer>();
-                selectionRenderer.material = defaultMaterial;
-                _selection = null;
-                selectedObject.GetComponent<DisplayMenu>().HideMenu();
-            }
-
-            var cam = camera.transform;
-            var ray = new Ray(cam.position, cam.forward);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                var selection = hit.transform;
-                if (selection.CompareTag(selectableTag))
+                if (_selection != null)
                 {
-                    var selectionRenderer = selection.GetComponent<Renderer>();
-                    if (selectionRenderer != null)
+                    var selectionRenderer = _selection.GetComponent<Renderer>();
+                    selectionRenderer.material = defaultMaterial;
+                    _selection = null;
+                    selectedObject.GetComponent<DisplayMenu>().HideMenu();
+                }
+    
+                var cam = camera.transform;
+                var ray = new Ray(cam.position, cam.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var selection = hit.transform;
+                    if (selection.CompareTag(selectableTag))
                     {
-                        selectionRenderer.material = highlightMaterial;
-                        selectedObject = hit.collider.gameObject;
-                        selectedObject.GetComponent<DisplayMenu>().ShowMenu();
+                        var selectionRenderer = selection.GetComponent<Renderer>();
+                        if (selectionRenderer != null)
+                        {
+                            selectionRenderer.material = highlightMaterial;
+                            selectedObject = hit.collider.gameObject;
+                            selectedObject.GetComponent<DisplayMenu>().ShowMenu();
+                        }
+    
+                        _selection = selection;
                     }
+                }
+            }
+            
+        }
 
-                    _selection = selection;
+        public void SelectionLockToggle()
+        {
+            Debug.Log("Selection Lock Toggle");
+            if (selectionLocked)
+            {
+                selectionLocked = false;
+                aimingDot.SetActive(true);
+            }
+            else
+            {
+                if (_selection != null)
+                {
+                    selectionLocked = true;
+                    aimingDot.SetActive(false);
                 }
             }
         }
