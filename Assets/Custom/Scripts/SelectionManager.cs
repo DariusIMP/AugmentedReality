14c0742bd77ebee17c0fecc54aa65eda16b031d1
Darius <dariusmaitia@gmail.com>
2019-08-14 19:15:55 -0300

    Control de la activación y desactivación del estado de seleccion

--- Assets/Custom/Scripts/SelectionManager.cs
+++ Assets/Custom/Scripts/SelectionManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Custom.Scripts
 {
@@ -11,43 +12,82 @@ namespace Custom.Scripts
         private GameObject selectedObject;
         private Transform _selection;
         private bool selectionLocked;
-        
+        private bool displayInfoMode;
+        private FadingText _fadingText;
         public Camera camera;
         public GameObject aimingDot;
-    
+        public GameObject instructions;
+
+        private void Start()
+        {
+            _fadingText = instructions.AddComponent<FadingText>();
+            _fadingText.textToShow = "Presione la pantalla para fijar un elemento";
+            _fadingText.holdingTime = 0.5f;
+            _fadingText.fadingTime = 1f;
+        }
+
         private void Update()
         {
-            if (!selectionLocked)
+            if (displayInfoMode)
             {
-                if (_selection != null)
+                if (!selectionLocked)
                 {
-                    var selectionRenderer = _selection.GetComponent<Renderer>();
-                    selectionRenderer.material = defaultMaterial;
-                    _selection = null;
-                    selectedObject.GetComponent<DisplayMenu>().HideMenu();
-                }
-    
-                var cam = camera.transform;
-                var ray = new Ray(cam.position, cam.forward);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    var selection = hit.transform;
-                    if (selection.CompareTag(selectableTag))
+                    if (_selection != null)
                     {
-                        var selectionRenderer = selection.GetComponent<Renderer>();
-                        if (selectionRenderer != null)
-                        {
-                            selectionRenderer.material = highlightMaterial;
-                            selectedObject = hit.collider.gameObject;
-                            selectedObject.GetComponent<DisplayMenu>().ShowMenu();
-                        }
-    
-                        _selection = selection;
+                        DeselectObject();
+                    }
+                    var cam = camera.transform;
+                    var ray = new Ray(cam.position, cam.forward);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        SelectObject(hit);
                     }
                 }
             }
-            
+        }
+
+        private void DeselectObject()
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+            selectedObject.GetComponent<DisplayMenu>().HideMenu();
+        }
+
+        private void SelectObject(RaycastHit hit)
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                    selectedObject = hit.collider.gameObject;
+                    selectedObject.GetComponent<DisplayMenu>().ShowMenu();
+                }
+    
+                _selection = selection;
+            }
+        }
+        public void InfoDisplayModeToggle()
+        {
+            if (displayInfoMode)
+            {
+                if (_selection != null)
+                {
+                    DeselectObject();
+                }
+                aimingDot.SetActive(false);
+                displayInfoMode = false;
+            }
+            else
+            {
+                _fadingText.ShowFadingText();
+                displayInfoMode = true;
+                aimingDot.SetActive(true);
+            }
         }
 
         public void SelectionLockToggle()
---
+++ Assets/Custom/Scripts/ToggleController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Custom.Scripts
+{
+    public class ToggleController : MonoBehaviour
+    {
+    
+        private Toggle toggle;
+ 
+        private void Start()
+        {
+            toggle = GetComponent<Toggle>();
+            toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+ 
+        private void OnToggleValueChanged(bool isOn)
+        {
+            ColorBlock cb = toggle.colors;
+            if (isOn)
+            {
+                cb.normalColor = Color.red;
+                cb.highlightedColor = Color.red;
+            }
+            else
+            {
+                cb.normalColor = Color.white;
+                cb.highlightedColor = Color.white;
+            }
+            toggle.colors = cb;
+        }
+    }
+}
