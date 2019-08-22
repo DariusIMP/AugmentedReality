93d69da426aacb90f6295bbcd1f0dcb3e85ad4ea
Juan Manuel Lambre <juanmlambre@gmail.com>
2019-08-22 14:27:05 -0300

    Tweek en decimales densidad

--- Assets/SliderValue.cs
+++ Assets/SliderValue.cs
@@ -8,6 +8,7 @@ public class SliderValue : MonoBehaviour
 
     public string Unit;
     public float Factor = 1;
+    public int Decimals = 2;
 
 
     // Start is called before the first frame update
@@ -19,7 +20,8 @@ public class SliderValue : MonoBehaviour
     public void UpdateValue()
     {
         double value = gameObject.GetComponentInParent<Slider>().value;
-        gameObject.GetComponent<Text>().text = string.Format("{0:0.00}{1}", value * Factor, Unit);
+        string format = "{0:0." + new string('0', Decimals) + "}{1}";
+        gameObject.GetComponent<Text>().text = string.Format(format, value * Factor, Unit);
     }
 
 }
