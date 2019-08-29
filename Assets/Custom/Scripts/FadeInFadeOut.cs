417519cf78e65f0ed4bd9cbab9f1e4d5c3f001f0
Darius <dariusmaitia@gmail.com>
2019-08-29 00:23:04 -0300

    Fade in fade out effect script

---
+++ Assets/Custom/Scripts/FadeInFadeOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    private Light _light;
+
+    private bool crescendo;
+
+    public float maxIntensity;
+
+    public float minIntensity;
+
+    public float multiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crescendo)
+        {
+            _light.intensity += Time.deltaTime * multiplier;
+            crescendo = _light.intensity < maxIntensity;
+        }
+        else
+        {
+            _light.intensity -= Time.deltaTime * multiplier;
+            crescendo = _light.intensity < minIntensity;
+        }
+    }
+}
