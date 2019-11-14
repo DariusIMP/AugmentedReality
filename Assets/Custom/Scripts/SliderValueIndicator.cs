1cd545e300593dbce277a9c0a811ddf8aca57b21
Darius <dariusmaitia@gmail.com>
2019-11-14 02:49:49 -0300

    Slider value indicator for indication of the trigger level and the vertical displacement

---
+++ Assets/Custom/Scripts/SliderValueIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueIndicator : MonoBehaviour
+{
+    public Text text;
+    public Slider slider;
+    
+    public void Start()
+    {
+        SetIndicator();
+    }
+
+    public void UpdateSliderValue()
+    {
+        SetIndicator();
+    }
+
+    private void SetIndicator()
+    {
+        text.text = Math.Round(slider.value, 2) + "%";
+    }
+}
